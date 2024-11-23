381528f3a3f10b29db75147071f0accef004fe18
Rudar08 <164479754+Rudar08@users.noreply.github.com>
2024-11-23 15:40:55 +0530

    backend server

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// Serve static files from the current directory
+app.use(express.static(path.join(__dirname, '../frontend')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/index.html'));
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Error in Fetching the Page');
+});
+
+
+app.use((req, res) => {
+    res.status(404).send('Sorry, page not found!');
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
